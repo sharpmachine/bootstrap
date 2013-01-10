73c21d6cf47d75ad19d2de157dd3c324037e4644
Jesse Kade <jessekade@gmail.com>
2013-01-10 11:44:05 -0800

    Update wp-content/themes/bootstrap/header.php
    Updated Title PHP

--- wp-content/themes/bootstrap/header.php
+++ wp-content/themes/bootstrap/header.php
@@ -15,16 +15,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    	<title><?php
-			global $page, $paged;
-			wp_title( '|', true, 'right' );
-			bloginfo( 'name' );
-			$site_description = get_bloginfo( 'description', 'display' );
-			if ( $site_description && ( is_home() || is_front_page() ) )
-				echo " | $site_description";
-			if ( $paged >= 2 || $page >= 2 )
-				echo ' | ' . sprintf( __( 'Page %s', 'smm' ), max( $paged, $page ) );
-			?></title>
+    	<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
     	<meta name="description" content="">
     	<meta name="author" content="Sharp Machine Media">
